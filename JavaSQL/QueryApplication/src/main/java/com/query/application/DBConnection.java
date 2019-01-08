2bcf0593c3801215feba512d2fd1d24cc89bfae4
trevor78 <36665098+trevor78@users.noreply.github.com>
2019-01-08 16:16:11 +0530

    Update DBConnection.java

--- JavaSQL/QueryApplication/src/main/java/com/query/application/DBConnection.java
+++ JavaSQL/QueryApplication/src/main/java/com/query/application/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/Warehouse_Receipts";
+	static final String DB_URL = "jdbc:mysql://WHSE_01/warehouse_receipts";
 
 	// Database credentials
 	static final String USER = "username";
